b9b6f65947170e1915133af0ebef3c7cc25446a5
Interzoneism <martinforslund.studio@gmail.com>
2025-09-27 14:54:52 +0200

    Add project files.

---
+++ ImprovedModMenu/ImprovedModMenuModSystem.cs
@@ -0,0 +1,13 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace ImprovedModMenu
+{
+    public class ImprovedModMenuModSystem : ModSystem
+    {
+
+
+    }
+}
